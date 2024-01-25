997f5c6e8a5435bbf9399a717a1c0ff490fd2ffb
Stavros Grigoriou <unix121@protonmail.com>
2024-01-25 22:49:20 +0200

    fix(build): adds build script, removes unwanted files (#2)

---
+++ release.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+# Script that builds the wheel of the project and creates a release
+#
+# @author: Stavros Grigoriou <unix121@protonmail.com>
+
+set -x
+set -eo pipefail
+
+RELEASE=${RELEASE:=false}
+
+>&2 echo "Building wheel."
+
+python3 setup.py sdist bdist_wheel
+
+twine check dist/*
+
+if [[ $RELEASE = true ]]; then
+  >&2 echo "Uploading release"
+  twine upload dist/*
+fi
