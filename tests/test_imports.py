3db32d4560cbdd4ca314d00acd56b3aa60263516
Stavros Grigoriou <unix121@protonmail.com>
2024-01-25 16:30:36 +0200

    release: v0.0.1-alpha (#1)
    * adds mongo connection module
    * adds xcom manager module

---
+++ tests/test_imports.py
@@ -0,0 +1,15 @@
+from unittest import TestCase, main
+
+"""
+@author: Stavros Grigoriou <unix121@protonmail.com>
+"""
+
+
+class ImportTest(TestCase):
+
+    def test_imports_should_succeed(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == "__main__":
+    main()
